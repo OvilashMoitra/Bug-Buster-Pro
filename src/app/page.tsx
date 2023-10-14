f2950d69d132750679973d892695937fc93f373e
Ovilash Moitra <96794196+OvilashMoitra@users.noreply.github.com>
2023-10-14 11:48:45 +0600

    blog card section done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,24 @@ import Footer from '../components/ui/Footer'
 import Header from '../components/ui/Header'
 import PriceSection from '@/components/ui/PriceSection'
 import Stats from '@/components/ui/Stats'
+import BlogCard from '@/components/ui/Blog/BlogCard'
 
 export default function Home() {
   return (
-    <>
+    <div className='bg-white'>
       <Navbar />
       {/* <Header /> */}
       <PriceSection />
       <Stats />
+      <div className='grid grid-cols-4 gap-5 px-5'>
+        <BlogCard />
+        <BlogCard />
+        <BlogCard />
+        <BlogCard />
+        <BlogCard />
+      </div>
+
       <Footer />
-    </>
+    </div>
   )
 }
