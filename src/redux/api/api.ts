4393c4724e2e4fc2daecbfc34ca9383b891a7ba2
Ovilash Moitra <96794196+OvilashMoitra@users.noreply.github.com>
2023-10-16 05:18:14 +0600

    redux added

---
+++ src/redux/api/api.ts
@@ -0,0 +1,11 @@
+import { axiosBaseQuery } from '@/lib/axiosBaseQuery'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+
+const baseUrl = "http://localhost:8000/api/v1/"
+// Define a service using a base URL and expected endpoints
+export const bugBusterProApi = createApi({
+    reducerPath: 'bugBusterProApi',
+    baseQuery: axiosBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({}),
+})
