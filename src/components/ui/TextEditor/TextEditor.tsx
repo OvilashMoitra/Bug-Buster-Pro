e50e72a1b48413f6c2196b870fdc812cbff85a07
Ovilash Moitra <96794196+OvilashMoitra@users.noreply.github.com>
2023-10-20 01:54:09 +0600

    jodit dynamic imported

--- src/components/ui/TextEditor/TextEditor.tsx
+++ src/components/ui/TextEditor/TextEditor.tsx
@@ -1,5 +1,14 @@
+"use client"
 import React, { useState, useRef, useMemo } from 'react';
-import JoditEditor from 'jodit-react';
+// import JoditEditor from 'jodit-react';
+
+import dynamic from 'next/dynamic';
+
+// This component will be rendered only on the client side
+const JoditEditor = dynamic(() => import('jodit-react'), {
+  loading: () => <p>Loading...</p>, // Optional loading component
+  ssr: false, // Disable SSR for this component
+});
 
 const TextEditor = ({content,setContent}:{content:string,setContent:(text:string)=>void}) => {
 	const editor = useRef(null);
