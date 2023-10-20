27cbdcf0242489ba80a5f5ea1d1059b744e044ba
Ovilash Moitra <96794196+OvilashMoitra@users.noreply.github.com>
2023-10-20 06:52:15 +0600

    remote server url added

--- src/redux/api/api.ts
+++ src/redux/api/api.ts
@@ -2,7 +2,7 @@ import { axiosBaseQuery } from '@/lib/axiosBaseQuery'
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 
-const baseUrl = "http://localhost:8000/api/v1"
+const baseUrl = "https://bug-buster-pro-backend.vercel.app/api/v1"
 // Define a service using a base URL and expected endpoints
 export const bugBusterProApi = createApi({
     reducerPath: 'bugBusterProApi',
