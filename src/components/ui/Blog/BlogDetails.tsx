9b12ede1b11ad11b2c47267c7f86faa2d166d251
Ovilash Moitra <96794196+OvilashMoitra@users.noreply.github.com>
2023-11-27 15:06:33 +0600

    blog funcality added

--- src/components/ui/Blog/BlogDetails.tsx
+++ src/components/ui/Blog/BlogDetails.tsx
@@ -57,11 +57,11 @@ const BlogDetails = ({ id }: { id: string }) => {
                     </div>
                     {/* <!-- End Content --> */}
 
-                    {/*//! <!-- Sidebar --> */}
+                    {/*
                     <div className="lg:col-span-1 lg:w-full lg:h-full lg:bg-gradient-to-r lg:from-gray-50 lg:via-transparent lg:to-transparent dark:from-slate-800">
                         <div className="sticky top-0 left-0 py-8 lg:pl-4 lg:pl-8">
                             {/* <!-- Avatar Media --> */}
-                            <div className="group flex items-center gap-x-3 border-b border-gray-200 pb-8 mb-8 dark:border-gray-700">
+                            {/* <div className="group flex items-center gap-x-3 border-b border-gray-200 pb-8 mb-8 dark:border-gray-700">
                                 <a className="block flex-shrink-0" href="#">
                                     <img className="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1669837401587-f9a4cfe3126e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=320&h=320&q=80" alt="Image Description" />
                                 </a>
@@ -86,12 +86,12 @@ const BlogDetails = ({ id }: { id: string }) => {
                                         </button>
                                     </div>
                                 </div>
-                            </div>
+                            </div> */}
                             {/* <!-- End Avatar Media --> */}
 
-                            <div className="space-y-6">
+                            {/* <div className="space-y-6"> */}
                                 {/* <!-- Media --> */}
-                                <a className="group flex items-center gap-x-6" href="#">
+                                {/* <a className="group flex items-center gap-x-6" href="#">
                                     <div className="grow">
                                         <span className="text-sm font-bold text-gray-800 group-hover:text-blue-600 dark:text-gray-200 dark:group-hover:text-blue-500">
                                             5 Reasons to Not start a UX Designer Career in 2022/2023
@@ -101,11 +101,11 @@ const BlogDetails = ({ id }: { id: string }) => {
                                     <div className="flex-shrink-0 relative rounded-lg overflow-hidden w-20 h-20">
                                         <img className="w-full h-full absolute top-0 left-0 object-cover rounded-lg" src="https://images.unsplash.com/photo-1567016526105-22da7c13161a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80" alt="Image Description" />
                                     </div>
-                                </a>
+                                </a> */}
                                 {/* <!-- End Media --> */}
 
                                 {/* <!-- Media --> */}
-                                <a className="group flex items-center gap-x-6" href="#">
+                                {/* <a className="group flex items-center gap-x-6" href="#">
                                     <div className="grow">
                                         <span className="text-sm font-bold text-gray-800 group-hover:text-blue-600 dark:text-gray-200 dark:group-hover:text-blue-500">
                                             If your UX Portfolio has this 20% Well Done, it Will Give You an 80% Result
@@ -115,11 +115,11 @@ const BlogDetails = ({ id }: { id: string }) => {
                                     <div className="flex-shrink-0 relative rounded-lg overflow-hidden w-20 h-20">
                                         <img className="w-full h-full absolute top-0 left-0 object-cover rounded-lg" src="https://images.unsplash.com/photo-1542125387-c71274d94f0a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80" alt="Image Description" />
                                     </div>
-                                </a>
+                                </a> */}
                                 {/* <!-- End Media --> */}
 
                                 {/* <!-- Media --> */}
-                                <a className="group flex items-center gap-x-6" href="#">
+                                {/* <a className="group flex items-center gap-x-6" href="#">
                                     <div className="grow">
                                         <span className="text-sm font-bold text-gray-800 group-hover:text-blue-600 dark:text-gray-200 dark:group-hover:text-blue-500">
                                             7 Principles of Icon Design
@@ -129,12 +129,12 @@ const BlogDetails = ({ id }: { id: string }) => {
                                     <div className="flex-shrink-0 relative rounded-lg overflow-hidden w-20 h-20">
                                         <img className="w-full h-full absolute top-0 left-0 object-cover rounded-lg" src="https://images.unsplash.com/photo-1586232702178-f044c5f4d4b7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80" alt="Image Description" />
                                     </div>
-                                </a>
+                                </a> */}
                                 {/* <!-- End Media --> */}
-                            </div>
-                        </div>
-                    </div>
-                    {/* <!-- End Sidebar --> */}
+                            {/* </div> */}
+                        {/* </div> */}
+                    {/* </div> */}
+                    
                 </div>
             </div>
             {/* <!--End Blog Article-- > */}
